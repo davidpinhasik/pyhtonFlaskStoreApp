004f20388d8be636a8754a4246cc0e3f19247d88
DAVID PINHASIK <pinhasik@il.ibm.com>
2019-12-22 13:47:38 +0200

    Added Postgres environment variable and psycopg2.

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+import os
+
 from flask import Flask
 from flask_restful import Api
 from flask_jwt import JWT
@@ -17,7 +19,7 @@ All resources that we define are classes.
 """
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', 'sqlite:///data.db')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False  # This just turns off the flask SQLAlchemy mod tracker
 # but not the SQLAlchemy mod tracker
 app.secret_key = 'somesecretkeyb'
